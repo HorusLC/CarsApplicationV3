87d633191adef9700ad5a31552f0031f2e470c72
HorusLC <dkostadinov_11@abv.bg>
2018-07-11 02:48:53 +0300

    Added enum visualization in combobox of UploadVehicleView

--- CarsApplicationV3.1/CarsApplicationV3/ViewModel/UploadVehicleVM.cs
+++ CarsApplicationV3.1/CarsApplicationV3/ViewModel/UploadVehicleVM.cs
@@ -1,10 +1,12 @@
 ﻿using CarsApplicationV3.Models;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CarsApplicationV3.ViewModel
 {
@@ -13,6 +15,8 @@ namespace CarsApplicationV3.ViewModel
     {
         private VehicleType type;
         private VehicleType selectedType;
+
+        public ICommand UploadVehicle { get; private set; }
         public VehicleType SelectedType
         {
             get { return selectedType; }
@@ -20,14 +24,24 @@ namespace CarsApplicationV3.ViewModel
         }
         public List<VehicleType> Type
         {
-            get;// { /*return type;*/ }
-            set; //{ /*type = value;*/ }
+            get;
+            set; 
         }
        public UploadVehicleVM()
         {
+            //TODO refactor Enum init, because of slow initilization through Casting
            Type= Enum.GetValues(typeof(VehicleType)).Cast<VehicleType>().ToList();
-
+            //Consider using ObjectDataProvider in XAML
+            
+            UploadVehicle = new RelayCommand(() => UploadVehicleF());
 
         }
+
+       private void UploadVehicleF()
+        {
+            if (selectedType == VehicleType.CAR) { SelectedType = VehicleType.MOTORCYCLE; }
+        }
+
+
     }
 }
